951ebb1f34cd9ec25420be0621edb46d670c8a5f
NGOGA Adolphe <adolphengoga@gmail.com>
2024-08-27 09:04:27 +0200

    adding blog route

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,10 +1,10 @@
-// import Router from 'express';
-// import asyncErrorHandler from '../utils/asyncErrorHandler';
-// import blogController from '../controllers/blogController';
+import Router from 'express';
+import asyncErrorHandler from '../utils/asyncErrorHandler';
+import blogController from '../controllers/blogController';
 
-// const router = Router();
+const router = Router();
 
-// router.get('/blog', asyncErrorHandler(blogController.getAllBlogs))
-// router.get('/blog/:id', asyncErrorHandler(blogController.getOneBlog))
+router.get('/blog', asyncErrorHandler(blogController.getAllBlogs))
+router.get('/blog/:id', asyncErrorHandler(blogController.getOneBlog))
 
-// export default router
+export default router
