53e09547d55b606ca6fc428fadbf2c765d875603
NGOGA Adolphe <adolphengoga@gmail.com>
2024-08-28 11:22:47 +0200

    adding paginator to searched data

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,6 +1,7 @@
 /* eslint-disable require-jsdoc */
 import { Sequelize } from "sequelize"
 import propertiesDB from "../utils/db/propertiesDB"
+import paginator from "../utils/paginator"
 
 async function searchController(req, res) {
   let { location, property_type, price, property_size } = req.query
@@ -9,10 +10,12 @@ async function searchController(req, res) {
   if (!price) price = []
   if (!property_size) property_size = []
   const results = await propertiesDB.searchProperty(location, property_type, price, property_size)
+  const page = req.query.page || 1
+  const limit = req.query.limit || 5
   res.status(200).json({
     status: 200,
     message: "search completed successfully",
-    data: results
+    data: paginator(results, page, limit)
   })
 }
 
