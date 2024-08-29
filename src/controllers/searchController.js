32ce8cc6f95d7439d5ca61a01ef1564a95e15cc9
NGOGA Adolphe <adolphengoga@gmail.com>
2024-08-29 10:32:44 +0200

    adding a seach by price range functionality

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -7,8 +7,14 @@ async function searchController(req, res) {
   let { location, property_type, price, property_size } = req.query
   if (!location) location = "null"
   if (!property_type) property_type = []
-  if (!price) price = []
+  if (!price) {
+    price = []
+  } else {
+    price = price.slice(1, -1).split(',').map(Number)
+  }
   if (!property_size) property_size = []
+  console.log(price)
+  console.log(typeof (price[0]))
   const results = await propertiesDB.searchProperty(location, property_type, price, property_size)
   const page = req.query.page || 1
   const limit = req.query.limit || 5
