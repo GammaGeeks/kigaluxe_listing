34884e618134b0333a87122573b564d099913872
NGOGA Adolphe <adolphengoga@gmail.com>
2024-08-30 11:45:59 +0200

    adding a place update route

---
+++ src/routes/placeRotes.js
@@ -0,0 +1,10 @@
+import Router from 'express';
+import asyncErrorHandler from '../utils/asyncErrorHandler';
+import verifyToken from '../middlewares/tokenValidation';
+import placeController from '../controllers/placeController';
+
+const router = Router();
+
+router.patch('/place', verifyToken, asyncErrorHandler(placeController.updatePlace))
+
+export default router
