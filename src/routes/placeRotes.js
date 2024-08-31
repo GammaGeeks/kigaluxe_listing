c222a0fe62d35eae3f3bdf5c17583b867bee7fcc
NGOGA Adolphe <adolphengoga@gmail.com>
2024-08-31 08:59:16 +0200

    creating a route to get all places

--- src/routes/placeRotes.js
+++ src/routes/placeRotes.js
@@ -5,6 +5,7 @@ import placeController from '../controllers/placeController';
 
 const router = Router();
 
+router.get('/place', asyncErrorHandler(placeController.getAllPlace))
 router.post('/place', verifyToken, asyncErrorHandler(placeController.createPlace))
 router.patch('/place/:id', verifyToken, asyncErrorHandler(placeController.updatePlace))
 router.delete('/place/:id', verifyToken, asyncErrorHandler(placeController.deleteAPlace))
