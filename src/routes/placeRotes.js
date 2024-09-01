d5aad648ad42ba0c7d2cd25c511b14ffaaf14c39
NGOGA Adolphe <adolphengoga@gmail.com>
2024-09-01 10:29:05 +0200

    adding a route to post place img

--- src/routes/placeRotes.js
+++ src/routes/placeRotes.js
@@ -1,12 +1,16 @@
 import Router from 'express';
+import multer from 'multer'
 import asyncErrorHandler from '../utils/asyncErrorHandler';
 import verifyToken from '../middlewares/tokenValidation';
 import placeController from '../controllers/placeController';
 
 const router = Router();
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
 
 router.get('/place', asyncErrorHandler(placeController.getAllPlace))
 router.post('/place', verifyToken, asyncErrorHandler(placeController.createPlace))
+router.post('/place/img/:id', verifyToken, upload.array('avatar'), asyncErrorHandler(placeController.addPlaceImg))
 router.patch('/place/:id', verifyToken, asyncErrorHandler(placeController.updatePlace))
 router.delete('/place/:id', verifyToken, asyncErrorHandler(placeController.deleteAPlace))
 
